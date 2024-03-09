63a8e32a826ce77ac9fe2f1181474540124b10ac
aadhar04 <70593940+aadhar04@users.noreply.github.com>
2024-03-09 21:45:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <nav>
+        <div className="logo">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/4517/5a94/d5389cc06b9d40c646878c60ac4bf7ed?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WdZ1ryUrbgZwgeefPDfNhH6563KlmUroKsEM6hRg6zvixSRI4gXy9a2NMleWU-uQuxPkaJBvmoN3YJp8JDxTSPoWq0JHmJJK2kP6kA~0t6yJ3ma9a~lQeDtyF5KT1VBlVyYQ~D4ogqewBybGSB-n4ToPa9diOR1Ora71ROmV8xxTtqjnm4RCSYQwLravvaq3HdlqJzK1rZ35aDx25A8wZxWJ4M1jSbdcb886Vn2F1dwygYVEcrXdRGSIcdMI~wjSsEWcNsla8A5bWkfANTR4RLMfmDh8ziwi32z2hZ1Ngk3BqEtd~oasgphsJ2uLbXT8~BaoBD7X8tiLkpBbykATEA__"
+            alt="logo svg image"
+          />
+        </div>
+        <div className="menu">
+          <div className="menu-item">MENU</div>
+          <div className="menu-item">LOCATION</div>
+          <div className="menu-item">ABOUT</div>
+          <div className="menu-item">CONTACT</div>
+        </div>
+        <div className="login">
+          <button>Login</button>
+        </div>
+      </nav>
+      <div className="frame2">
+        <div className="frame1">
+          <div className="lHead">YOUR FEET DESERVE THE BEST</div>
+          <div className="sHead">
+            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.
+          </div>
+          <div className="buttons">
+            <button className="button1">Shop Now</button>
+            <button className="button2">Category</button>
+          </div>
+          <div className="shop">
+            <div className="text">Also Available On</div>
+            <div className="buyLogo">
+              <img
+                src="https://s3-alpha-sig.figma.com/img/4331/d1d3/fe15236a1a9843e35f7b3b8467e998ff?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OMByJS6UtRDX9j19xYRK~ZdXBC6i86HzYyhfslEc78N3incYLp391fo5CdJj9FnLkMbQPgN6eXvhx9i7ccrGH08gmcN~8VAanE8ak2G845lwaMt~DVZ6g3NRHB78hGKQsjGE9wnH2-fGrTqbv7DFobZFCeYh-9pD4GWy--xs71rLXaFLu9GrE-Ln15hOlBqj~yxySX8h4dDjgg9YXqTd4GPku4KBTzsS6udJARznMcwoFynrVlBreDzBH-c6fqAt690y-KAfNSC7Q~ug3GeKWzSZtao1EJ42kxEWQVAP6v0liW9Mlkh~GLRlAADKZW-FwkvXv5ASIizqTzjOi2DKOA__"
+                alt="amazon logo"
+              />
+              <img
+                src="https://s3-alpha-sig.figma.com/img/9669/ba8f/4f98c0bc6952e9281673372fcb16cfc0?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RB5uDL4fWhBoYA2GgEECDX1vwAvphIxVJU~N7686DdxFClWvwq1MsaZHK2Ui~0kHWz51B6v94JjUGM0I34VbcD9tXqAzHEyCQNTEEmKlaRa6mU~dK58vZsL0ieaqiP3vaSqTXpzmVpdZr~EybWzgwNtTTREnfvK1OnD8LwmcxjwfCiZyJ-TNi5Dp~otT8fUhfAMJOHNO8RqTR4OKr6a3bdUT~sXOevq19YgPBDsT36R5m0n5WCSvqzYmsJqYPyZWTw~Em3J~mzPaJWB7IO5VKUHRi5zdTkOfL8qy6VSenO999ia-rJ4dJMPEhhb5il~Y~Dd4aa4PTDaXwcLFyARWGg__"
+                alt="flipkart logo"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="shoe">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/ccd9/e7f6/9d4cbfcf331c69b8e21db35d2e158410?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ns5Zv3xcPfg7C6Ku3fwgpeV~sq9okehKVLQL3UdPd0Rxx5LhRWaNB5o7-23z7b6rSFwDW1CK241pb4M-SI1Ta~GSBueLAtJAG0JS3D8XABenn-6aGFs94x9zFs0lKiWgBjyrdewkSud6ep2yVjJKmVmfzAI2aJsD5xaQZXHrAIT4cZTxf7BZTUt138ElOPJ83pwigK~idJ5PkM2UKZAj2HCUyV4c5cZXkCGpUdjpY5niC6-eqAKZxm-HcqBJ9fS7Hojb59Z2RkjKxqgEoGSt9-B9rOWUS~sQkiKVJp1EZAdxaitJzgob1SFyl6yLXyb2qr10c4kNVbW~LdJbzNMScQ__"
+            alt="shoe image"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
